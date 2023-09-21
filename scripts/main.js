4c575e95751c5f00d90ed71fd5dcc9f42c34cc43
kishi4web2021 <95006357+kishi4web2021@users.noreply.github.com>
2023-09-21 12:11:51 +0900

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -19,7 +19,7 @@ myButton.onclick = function() {
 let myImage = document.querySelector('img');
 
 
-myImage.onclick = () => {
+myImage.onclick = functon() {
   const mySrc = myImage.getAttribute("src");
   if (mySrc === "images/firefox-icon.png") {
     myImage.setAttribute("src", "images/firefox2.png");
